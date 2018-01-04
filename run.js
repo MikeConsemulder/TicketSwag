bf3d51234829fcbc271cc5c4a959accb7fa241cb
Mike <m.consemulder1992@gmail.com>
2018-01-04 14:44:46 +0100

    changed url to awakenings eindhoven

--- run.js
+++ run.js
@@ -3,7 +3,8 @@ var openUrl = require("openurl");
 const cheerio = require('cheerio');
 var $;
 
-var url = 'https://www.ticketswap.nl/event/gold-new-biki90-x-rgb-x-ketelhuis/ed9f37dc-1ff4-405a-aa34-80aa33b92246';
+var url = 'https://www.ticketswap.nl/event/awakenings-eindhoven/regular/9476ae2b-5622-4f02-8137-362abff371dd/541602';
+//url = 'https://www.ticketswap.nl/event/gold-new-biki90-x-rgb-x-ketelhuis/ed9f37dc-1ff4-405a-aa34-80aa33b92246';
 //url = 'https://www.ticketswap.nl/event/flawless-xxl/regular/ebbb410d-8df4-4dde-8b62-e7dff2a45e72/68187';
 
 var timer;
